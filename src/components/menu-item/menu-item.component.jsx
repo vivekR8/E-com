1d4fede76ff52130699714268796636fd005c5b0
Vivek Raghuvanshi <vivekraghuv8@gmail.com>
2020-07-18 20:32:42 +0530

    homepage complete

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./menu-item.component.scss";
+
+const menuItem = ({title,imageUrl,size})=>(
+    <div className={`${size} menu-item`}>
+        <div className='background-image'
+        style={{
+        backgroundImage: `url(${imageUrl})`
+    }} />
+        <div className="content">
+            <h1 className = 'title'>{title.toUpperCase()}</h1>
+            <span className = "subtitle">SHOP Now</span>          
+        </div>
+    </div>
+);
+export default menuItem;
