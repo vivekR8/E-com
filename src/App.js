c6e55b3e215834d0b7a63fad8bce04882c843f4e
Vivek Raghuvanshi <vivekraghuv8@gmail.com>
2020-07-23 14:43:52 +0530

    Reusable form input components and button component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Switch ,Route} from 'react-router-dom' 
+import {Switch ,Route} from 'react-router-dom';
+
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from "./pages/homepage.component.jsx";
-import ShopPage from "./pages/shop/shop.component"
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from './pages/signin_and_signup/signin_and_signup';
 
 
 
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact path='/' component ={HomePage} />
       <Route path='/shop' component = {ShopPage} />
+      <Route path='/signin' component = {SignInAndSignUp} />
     </Switch>
     </div>
   );
---
+++ src/pages/signin_and_signup/signin_and_signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './signin_and_signup.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUp = ()=>(
+    <div className='sign-in_and_sign-up'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUp;
